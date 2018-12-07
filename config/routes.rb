d4ca85f4a7a78b27e3f1b3136188d2b28e8168f5
B-Dominguez <bidominguez@uc.cl>
2018-12-07 14:36:16 -0300

    user profile view without map

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'search_person', to: "search_person#index"
   get 'search_person/results', to: "search_person#show"
   patch 'search_person', to: "search_person#update"
+  resources :users, only: [:index, :show]
   # buesqueda de trabajadores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
